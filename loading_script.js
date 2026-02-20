a8a3069d3f89cd21b187303fc0fef1843db5e003
Mark Rubin <mark.rubin@gmail.com>
2026-02-19 21:44:28 -0800

    Detecting the availability endpoint call and adding more.

--- loading_script.js
+++ loading_script.js
@@ -5,48 +5,70 @@
 // @description  Shows pickleball court booking opportunities across multiple clubs
 // @author       Mark Rubin
 // @match        https://bayclubconnect.com/*
-// @icon         https://github.com/mbrubin56gh/bayclubconnect_helper/blob/d4f3023bb29f8db0fc4799894a084bb01c81d49e/icons/pickleball_17155178.png
 // @run-at       document-start
-// @grant none
+// @icon         https://github.com/mbrubin56gh/bayclubconnect_helper/blob/d4f3023bb29f8db0fc4799894a084bb01c81d49e/icons/pickleball_17155178.png
+// @grant        none
 // ==/UserScript==
 
 (function () {
     'use strict';
 
     let capturedHeaders = {};
+    const originalSetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
+    XMLHttpRequest.prototype.setRequestHeader = function (name, value) {
+        if (name === 'Authorization' || name === 'X-SessionId') {
+            capturedHeaders[name] = value;
+        }
+        return originalSetRequestHeader.apply(this, arguments);
+    };
 
-    const originalFetch = window.fetch;
-    window.fetch = function (url, options = {}) {
-        if (url.includes('connect-api.bayclubs.io/court-booking/api/1.0/availability')) {
-            // Capture headers as before
-            const h = options.headers || {};
-            if (h['Authorization']) capturedHeaders['Authorization'] = h['Authorization'];
-            if (h['X-SessionId']) capturedHeaders['X-SessionId'] = h['X-SessionId'];
+    const originalXhrOpen = XMLHttpRequest.prototype.open;
+    const originalXhrSend = XMLHttpRequest.prototype.send;
 
-            // Parse the URL to extract the params the page is using
-            const parsedUrl = new URL(url);
+    XMLHttpRequest.prototype.open = function (method, url, ...rest) {
+        this._url = url; // stash the url for use in send
+        return originalXhrOpen.apply(this, [method, url, ...rest]);
+    };
+
+    XMLHttpRequest.prototype.send = function (body) {
+        if (this._url && this._url.includes('court-booking/api/1.0/availability')) {
+            alert("original call detected");
+            const parsedUrl = new URL(this._url);
             const date = parsedUrl.searchParams.get('date');
             const categoryCode = parsedUrl.searchParams.get('categoryCode');
             const categoryOptionsId = parsedUrl.searchParams.get('categoryOptionsId');
             const timeSlotId = parsedUrl.searchParams.get('timeSlotId');
 
-            // Fire your multi-club batch
-            fetchAllClubs({ date, categoryCode, categoryOptionsId, timeSlotId, headers: h });
-
-            // Still let the original request proceed so the page works normally
-            return originalFetch.apply(this, arguments);
+            fetchAllClubs(date, timeSlotId);
         }
-
-        return originalFetch.apply(this, arguments);
+        return originalXhrSend.apply(this, arguments);
     };
+    // const originalFetch = window.fetch;
+    // window.fetch = function (url, options = {}) {
+    //     alert(url);
+    //     if (url.includes('availability')) {
+    //         alert("original call detected")
+    //         // Capture headers as before
+    //         const h = options.headers || {};
+    //         if (h['Authorization']) capturedHeaders['Authorization'] = h['Authorization'];
+    //         if (h['X-SessionId']) capturedHeaders['X-SessionId'] = h['X-SessionId'];
 
-    const originalXhrOpen = XMLHttpRequest.prototype.setRequestHeader;
-    XMLHttpRequest.prototype.setRequestHeader = function (name, value) {
-        if (name === 'Authorization' || name === 'X-SessionId') {
-            capturedHeaders[name] = value;
-        }
-        return originalXhrOpen.apply(this, arguments);
-    };
+    //         // Parse the URL to extract the params the page is using
+    //         const parsedUrl = new URL(url);
+    //         const date = parsedUrl.searchParams.get('date');
+    //         const categoryCode = parsedUrl.searchParams.get('categoryCode');
+    //         const categoryOptionsId = parsedUrl.searchParams.get('categoryOptionsId');
+    //         const timeSlotId = parsedUrl.searchParams.get('timeSlotId');
+
+    //         // Fire your multi-club batch
+    //         fetchAllClubs({ date, categoryCode, categoryOptionsId, timeSlotId, headers: h });
+
+    //         // Still let the original request proceed so the page works normally
+    //         return originalFetch.apply(this, arguments);
+    //     }
+
+    //     return originalFetch.apply(this, arguments);
+    // };
 
     function onUrlChange(callback) {
         const originalPushState = history.pushState.bind(history);
@@ -76,10 +98,10 @@
     }
 
     function onRacquetSportsFilterNodeLoaded() {
-        alert("racquet sports filter loaded");
+        // alert("racquet sports filter loaded");
     }
 
-    function fetchAllClubs() {
+    function fetchAllClubs(selectedDate, timeSlotId) {
         const clubs = [
             '9a2ab1e6-bc97-4250-ac42-8cc8d97f9c63', // Broadway
             '95eb0299-b5cf-4a9f-8b35-e4b3bd505f18', // Redwood Shores
@@ -89,7 +111,7 @@
 
         (async () => {
             const results = await Promise.all(clubs.map(clubId =>
-                fetch(`https://connect-api.bayclubs.io/court-booking/api/1.0/availability?clubId=${clubId}&date=${selectedDate}&categoryCode=pickleball&categoryOptionsId=182a18e2-fd11-4868-a6be-36d96f7f2645&timeSlotId=ea57c6b1-069c-4df9-8ee6-0d63ade162bc`, {
+                fetch(`https://connect-api.bayclubs.io/court-booking/api/1.0/availability?clubId=${clubId}&date=${selectedDate}&categoryCode=pickleball&categoryOptionsId=182a18e2-fd11-4868-a6be-36d96f7f2645&timeSlotId=${timeSlotId}}`, {
                     headers: {
                         'Authorization': capturedHeaders['Authorization'],
                         'X-SessionId': capturedHeaders['X-SessionId'],
