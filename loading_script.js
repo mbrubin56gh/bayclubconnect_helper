e3772a2f3c989cc70f0db1bc861a794c1634d312
Mark Rubin <mark.rubin@gmail.com>
2026-02-19 19:11:13 -0800

    First detection of the reservation URL change.

---
+++ loading_script.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Bay Club Connect Multi-club Pickleball Court Reservation Helper
+// @namespace    https://github.com/mbrubin56gh
+// @version      0.1
+// @description  Show you Pickleball court booking opportunities across multiple clubs
+// @author       Mark Rubin
+// @match        https://bayclubconnect.com/*
+// @icon         https://github.com/mbrubin56gh/bayclubconnect_helper/blob/d4f3023bb29f8db0fc4799894a084bb01c81d49e/icons/pickleball_17155178.png
+// @require      http://code.jquery.com/jquery-latest.js
+// @grant none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function onUrlChange(callback) {
+        const originalPushState = history.pushState.bind(history);
+        const originalReplaceState = history.replaceState.bind(history);
+
+        history.pushState = function(...args) {
+            originalPushState(...args);
+            callback(location.href);
+        };
+        history.replaceState = function(...args) {
+            originalReplaceState(...args);
+            callback(location.href);
+        };
+
+        window.addEventListener('popstate', () => callback(location.href));
+    }
+
+    onUrlChange((url) => {
+        alert("URL HAS CHANGED: " + url);
+        if (url.includes('/racquet-sports/create-booking/')) {
+            alert("On reservation system");
+        }
+    });
+
+    alert("On home page");
+})();
