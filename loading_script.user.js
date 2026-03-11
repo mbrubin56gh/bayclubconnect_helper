5482bbfeef9b15d62be5af014d327b2eea4fd340
Mark Rubin <mark.rubin@gmail.com>
2026-03-10 21:32:24 -0700

    Revving the version

--- loading_script.user.js
+++ loading_script.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Bay Club Connect Pickleball Court Reservation Helper
 // @namespace    https://github.com/mbrubin56gh
-// @version      0.81
+// @version      1.00
 // @description  Shows pickleball court booking slots across multiple clubs
 // @author       Mark Rubin
 // @match        https://bayclubconnect.com/*
